72208a4b536ac2588e2721ac97e7d375810faad1
hiramaralia <hiramaral.ia@gmail.com>
2022-05-18 08:27:41 -0400

    Hiram update M3

---
+++ 04/hcca_random00.py
@@ -0,0 +1,10 @@
+import pandas as pd
+left = pd.DataFrame(
+    {
+        "key1": ["K0", "K0", "K1", "K2"],
+        "key2": ["K0", "K1", "K0", "K1"],
+        "A": ["A0", "A1", "A2", "A3"],
+        "B": ["B0", "B1", "B2", "B3"],
+    }
+)
+print(left)
